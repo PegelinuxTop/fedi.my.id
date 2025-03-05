8bf30ec7203c1d1c403feeff2812420838d0990b
Essem <smswessem@gmail.com>
2025-03-05 12:47:48 -0600

    Add list of reactions

--- app/javascript/flavours/glitch/components/account.tsx
+++ app/javascript/flavours/glitch/components/account.tsx
@@ -17,6 +17,7 @@ import { openModal } from 'flavours/glitch/actions/modal';
 import { initMuteModal } from 'flavours/glitch/actions/mutes';
 import { apiFollowAccount } from 'flavours/glitch/api/accounts';
 import { Avatar } from 'flavours/glitch/components/avatar';
+import { AvatarOverlay } from 'flavours/glitch/components/avatar_overlay';
 import { Button } from 'flavours/glitch/components/button';
 import { FollowersCounter } from 'flavours/glitch/components/counters';
 import { DisplayName } from 'flavours/glitch/components/display_name';
@@ -29,6 +30,7 @@ import { VerifiedBadge } from 'flavours/glitch/components/verified_badge';
 import { useIdentity } from 'flavours/glitch/identity_context';
 import { me } from 'flavours/glitch/initial_state';
 import type { MenuItem } from 'flavours/glitch/models/dropdown_menu';
+import type { StatusReaction } from 'flavours/glitch/models/reaction';
 import { useAppSelector, useAppDispatch } from 'flavours/glitch/store';
 
 import { Permalink } from './permalink';
@@ -65,12 +67,21 @@ const messages = defineMessages({
 
 export const Account: React.FC<{
   size?: number;
+  overlayEmoji?: StatusReaction;
   id: string;
   hidden?: boolean;
   minimal?: boolean;
   defaultAction?: 'block' | 'mute';
   withBio?: boolean;
-}> = ({ id, size = 46, hidden, minimal, defaultAction, withBio }) => {
+}> = ({
+  id,
+  size = 46,
+  overlayEmoji = { name: null },
+  hidden,
+  minimal,
+  defaultAction,
+  withBio,
+}) => {
   const intl = useIntl();
   const { signedIn } = useIdentity();
   const account = useAppSelector((state) => state.accounts.get(id));
@@ -251,6 +262,13 @@ export const Account: React.FC<{
     verification = <VerifiedBadge link={firstVerifiedField.value} />;
   }
 
+  let statusAvatar;
+  if (!overlayEmoji.name) {
+    statusAvatar = <Avatar account={account} size={size} />;
+  } else {
+    statusAvatar = <AvatarOverlay account={account} emoji={overlayEmoji} />;
+  }
+
   return (
     <div className={classNames('account', { 'account--minimal': minimal })}>
       <div className='account__wrapper'>
@@ -262,11 +280,7 @@ export const Account: React.FC<{
           data-hover-card-account={id}
         >
           <div className='account__avatar-wrapper'>
-            {account ? (
-              <Avatar account={account} size={size} />
-            ) : (
-              <Skeleton width={size} height={size} />
-            )}
+            {account ? statusAvatar : <Skeleton width={size} height={size} />}
           </div>
 
           <div className='account__contents'>
